5f49d11b26b0737b91101834545b8c1c51f08b5d
sohamgit96 <86605548+sohamgit96@users.noreply.github.com>
2024-10-06 14:27:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
-DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # this is jkust to help if you dont want to force your bot user to login or if they not interested
+API_ID = int(getenv("API_ID", "2895116"))
+API_HASH = getenv("API_HASH", "4372052ba951273fa2865c100a85cbb6")
+BOT_TOKEN = getenv("BOT_TOKEN", "1945745284:AAHHd9wgR3qpBw5gDD_hbFaVQOzIpLih5vw")
+OWNER_ID = int(getenv("OWNER_ID", "1496223596"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://umeshfake:umeshfake@cluster0.tzw05.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002266769148"))
+FORCESUB = getenv("FORCESUB", "https://t.me/+mIiePHQ7TqE4MWI5")
+DEFAULT_SESSION = getenv("DEFAULT_SESSION", "AQAsLQwAxMvosMFx3kZZYsEKQyAb74jeGTPKyLiWL7krBst3YsQy8s3Ocsa248CeXQrkjzEnyhuhYW-iOLth8YfHQD6d68vS5ZYPmHY5n077KpoUO1CQPZNJmGL5wI4WwQvPw8Mf06ZdBw8JW_nPuH0m3-dEazpTKEycsjuYIYoRCm2zbq58nTyv2B6AQ1C_0nE-SVMbtHWRxtfh7910UUFPcF7Apj-rtcv6JVIR1YKJzoUdklyCYKBLx-Q4_uHgRYjdlX0_phfm0xl7hgjuC9fgOJEYZdzeHlTFqG7dPIEL-Xpdu_2REHTPHQ-6iLsYVMyi-sUFRx0Md8FrkLGYcBrnvlWKdgAAAABZLo9sAA") # this is jkust to help if you dont want to force your bot user to login or if they not interested
